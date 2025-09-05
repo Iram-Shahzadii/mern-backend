2e3d428034242966a9f658e6932087020dcd808e
Iram Shahzadi <alizashahzadi151@gmail.com>
2025-09-05 13:38:09 +0500

    /routes

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ require('dotenv').config()
 const app = require('./src/app')
 
 
-
+app.get('/', (req, res) => {
+    res.send({
+        activeStatus: true,
+        error:false,
+    })
+        
+    })
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
 })
