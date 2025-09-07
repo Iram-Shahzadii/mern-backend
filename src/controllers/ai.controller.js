0321eb58485e790e5a6570fb287dd2cf81034ebe
Iram Shahzadi <alizashahzadi151@gmail.com>
2025-09-07 10:27:31 +0500

    new changes

--- src/controllers/ai.controller.js
+++ src/controllers/ai.controller.js
@@ -1,20 +1,40 @@
-const aiService = require("../services/ai.service")
+// const aiService = require("../services/ai.service")
 
+
+// module.exports.getReview = async (req, res) => {
+
+//     const code = req.body.code;
+
+//     if (!code) {
+//         return res.status(400).send("Prompt is required");
+//     }
+
+//     const response = await aiService(code);
+
+
+//     res.send(response);
+
+// }
+const aiService = require("../services/ai.service");
 
 module.exports.getReview = async (req, res) => {
+    try {
+        console.log("REQ HEADERS:", req.headers);  // Debugging
+        console.log("REQ BODY:", req.body);        // Debugging
 
-    const code = req.body.code;
+        const code = req.body?.code; // Safe access
 
-    if (!code) {
-        return res.status(400).send("Prompt is required");
+        if (!code) {
+            return res.status(400).json({ error: "Code is required", received: req.body });
+        }
+
+        const response = await aiService(code);
+
+        return res.json({ success: true, data: response });
+    } catch (error) {
+        console.error("Controller Error:", error);
+        return res.status(500).json({ error: "Something went wrong", details: error.message });
     }
-
-    const response = await aiService(code);
-
-
-    res.send(response);
-
-}
-
+};
 
 
