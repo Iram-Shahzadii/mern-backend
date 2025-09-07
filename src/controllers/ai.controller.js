b94ecfee0686b3eb40d2ee17f6afe2161ad7a6c5
Iram Shahzadi <alizashahzadi151@gmail.com>
2025-09-07 10:06:59 +0500

    add new

--- src/controllers/ai.controller.js
+++ src/controllers/ai.controller.js
@@ -1,23 +1,20 @@
-// const aiService = require("../services/ai.service")
+const aiService = require("../services/ai.service")
 
 
-// module.exports.getReview = async (req, res) => {
-
-//     const code = req.body.code;
-
-//     if (!code) {
-//         return res.status(400).send("Prompt is required");
-//     }
-
-//     const response = await aiService(code);
-
-
-//     res.send(response);
-
-// }
 module.exports.getReview = async (req, res) => {
-    console.log("REQ BODY:", req.body);
-    res.json({ received: req.body });
+
+    const code = req.body.code;
+
+    if (!code) {
+        return res.status(400).send("Prompt is required");
+    }
+
+    const response = await aiService(code);
+
+
+    res.send(response);
+
 }
+
 
 
